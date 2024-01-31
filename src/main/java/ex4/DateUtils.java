2f650f763e14511c352135945e570a43ed1deb09
Ramzi <ramzibenmansour.pro@gmail.com>
2024-01-31 11:42:00 +0100

    exo 4 done

--- src/main/java/ex4/DateUtils.java
+++ src/main/java/ex4/DateUtils.java
@@ -5,15 +5,42 @@ import java.util.Date;
 
 public class DateUtils {
 
+	/** Le modèle de format par défaut pour les dates. */
+	private static final String DEFAULT_PATTERN = "dd/MM/yyyy HH:mm:ss";
+
+	/**
+	 * Formate une date en utilisant un modèle de format personnalisé.
+	 *
+	 * @param pattern Le modèle de format à utiliser.
+	 * @param date    La date à formater.
+	 * @return La date formatée sous forme de chaîne de caractères.
+	 */
 	public static String format(String pattern, Date date) {
-		
 		SimpleDateFormat formateur = new SimpleDateFormat(pattern);
-		return formateur.format(date);
+		return formaterDate(formateur, date);
 	}
-	
+
+	/**
+	 * Formate une date en utilisant le modèle de format par défaut.
+	 *
+	 * @param date La date à formater.
+	 * @return La date formatée sous forme de chaîne de caractères en utilisant le
+	 *         modèle par défaut.
+	 */
 	public static String formatDefaut(Date date) {
-		
-		SimpleDateFormat formateur = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		SimpleDateFormat formateur = new SimpleDateFormat(DEFAULT_PATTERN);
+		return formaterDate(formateur, date);
+	}
+
+	/**
+	 * Formate une date en utilisant le formateur de date spécifié.
+	 *
+	 * @param formateur Le formateur de date à utiliser.
+	 * @param date      La date à formater.
+	 * @return La date formatée sous forme de chaîne de caractères.
+	 */
+	private static String formaterDate(SimpleDateFormat formateur,
+			Date date) {
 		return formateur.format(date);
 	}
 }
